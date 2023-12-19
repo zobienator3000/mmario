5103f1bda8a0f64aefa5be7129511457740d77c3
stas <stasrudenok11@gmail.com>
2023-12-19 15:20:17 +0300

    m

--- Assets/Scenes/Script/Heart.cs
+++ Assets/Scenes/Script/Heart.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Heart : MonoBehaviour
+public class heart : MonoBehaviour
 {
     [SerializeField] private float heartValue;
 
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" && !collision.GetComponent<Health>().isHealthMax())
         {
-            collision.GetComponent<health>().addHeath(heartValue);
+            collision.GetComponent<Health>().addHeath(heartValue);
             gameObject.SetActive(false);
         }
     }
