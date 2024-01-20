473d5f183b98e631393894fdd3009a6e28a4ea57
stas <stasrudenok11@gmail.com>
2024-01-20 16:16:50 +0300

    l

--- Assets/Scenes/Script/Enemy.cs
+++ Assets/Scenes/Script/Enemy.cs
@@ -54,13 +54,23 @@ public class Enemy : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        //if (collision.tag == "Player" && health > 0)
+        //{
+        //    collision.GetComponent<Health>().TakeDamage(damage);
+        //}
+        if (collision.tag == "Bullet")
         {
-            collision.GetComponent<Health>().TakeDamage(damage);
-        }
-        if (collision.CompareTag("Bullet"))
-        {
-            health = health - 1;
+            damaged();
         }
     }
+    IEnumerator Die()
+    {
+        animator.Play("enemy_die");
+        transform.position = new Vector2(transform.position.x, yy);
+        yield return new WaitForSeconds(1f);
+        gameObject.SetActive(false);
+        Instantiate(DroppedCoin, transform.position, transform.rotation);
+
+        void health()
+     }
 }
